eb2eb0eeb19cd3a79a4f5654fc0ffa3f440ecf1d
caleb lee <caleb.lee@my.wheaton.edu>
2023-04-27 13:58:20 -0500

    Leaderboard stuff

---
+++ app/src/main/java/com/example/scavenger/HuntArrayAdapter.java
@@ -0,0 +1,72 @@
+package com.example.scavenger;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class HuntArrayAdapter extends ArrayAdapter<Hunt> {
+    Fragment currentFragment;
+
+    // code modified from https://www.geeksforgeeks.org/gridview-in-android-with-example/#
+
+    public HuntArrayAdapter(@NonNull Context context, ArrayList<Hunt> huntArrayList, Fragment fragment) {
+        super(context, 0, huntArrayList);
+        currentFragment = fragment;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if (listItemView == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.hunt_card_view, parent, false);
+        }
+
+        // get the hunt and set the hunt name and description text
+        Hunt hunt = getItem(position);
+        TextView huntnameTV = listItemView.findViewById(R.id.huntname);
+        TextView huntdescTV = listItemView.findViewById(R.id.huntdesc);
+        ImageView cardbg = listItemView.findViewById(R.id.huntcardbg);
+        listItemView.findViewById(R.id.edithuntbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                leaderboardHunt(hunt);
+            }
+        });
+
+        huntnameTV.setText(hunt.getName());
+        huntdescTV.setText(hunt.getDesc());
+        if (hunt.getbgcolor().equalsIgnoreCase("Blue")) cardbg.setImageResource(R.drawable.bluebg);
+        else if (hunt.getbgcolor().equalsIgnoreCase("Orange")) cardbg.setImageResource(R.drawable.orangebg);
+        else cardbg.setImageResource(R.drawable.redbg);
+        return listItemView;
+    }
+
+    /**
+     * Navigate to the LeaderboardFragment
+     * @post Leaderboard.hunt = hunt
+     * @param hunt the Hunt that is being edited
+     */
+    private void leaderboardHunt(Hunt hunt) {
+        EditHuntFragment.hunt = hunt;
+        NavHostFragment.findNavController(currentFragment)
+                .navigate(R.id.); // LeaderboardMainFragment to LeaderboardFragment
+    }
+
+}
--- app/src/main/java/com/example/scavenger/LeaderboardMainFragment.java
+++ app/src/main/java/com/example/scavenger/LeaderboardMainFragment.java
@@ -1,16 +1,22 @@
 package com.example.scavenger;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.scavenger.databinding.FragmentLeaderboardMainBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -18,33 +24,31 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LeaderboardActivity extends AppCompatActivity {
+public class LeaderboardMainFragment extends Fragment {
 
-    ListView listView;
-    HuntArrayAdapter arrayAdapter;
-    Hunt[] huntList = new Hunt[0];
+    private FragmentLeaderboardMainBinding binding;
+
+    private SearchView huntSV;
+
+    HuntArrayAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leaderboard);
-
-        listView = findViewById(R.id.listView);
-        arrayAdapter = new HuntArrayAdapter();
-        listView.setAdapter(arrayAdapter);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        binding = FragmentLeaderboardMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
-    // outside onCreate
-
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.actionbar_menu, menu);
         MenuItem menuItem = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Search here");
+        huntSV = (SearchView) menuItem.getActionView();
+        huntSV.setQueryHint("Search here");
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        huntSV.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -52,14 +56,26 @@ public class LeaderboardActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
+                adapter.getFilter().filter(newText);
                 return false;
             }
         });
-
-        return super.onCreateOptionsMenu(menu);
     }
-    private void displayHunts() {
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        MenuItem item=menu.findItem(R.id.search);
+        item.setVisible(false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getView().setBackgroundColor(getResources().getColor(R.color.avocado));
+
+        displayLeaderboards();
+    }
+
+    private void displayLeaderboards() {
         FirebaseFirestore.getInstance().collection("Hunts").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
@@ -74,8 +90,8 @@ public class LeaderboardActivity extends AppCompatActivity {
                                 if (d.toObject(Hunt.class).getCreator().equalsIgnoreCase(userEmail))
                                     huntArrayList.add(d.toObject(Hunt.class));
                             }
-                            HuntGVAdapter adapter = new HuntGVAdapter(getActivity(), huntArrayList, CreatorHomePageFragment.this);
-                            huntGV.setAdapter(adapter);
+                            adapter = new HuntArrayAdapter(getActivity(), huntArrayList, LeaderboardMainFragment.this);
+                            huntSV.setAdapter(adapter);
                         } else {
                             System.out.println("Hunts database collection is empty!");
                             // maybe display something that says "no hunts created yet"?
