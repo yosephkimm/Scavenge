d55eaf1f032804b39ecc4c828bb7c4cca6f680b2
caleb lee <caleb.lee@my.wheaton.edu>
2023-04-28 06:05:56 -0500

    Finished bgs

--- app/src/main/java/com/example/scavenger/leaderboardfiles/PlayerTimeAdapter.java
+++ app/src/main/java/com/example/scavenger/leaderboardfiles/PlayerTimeAdapter.java
@@ -1,4 +1,4 @@
-package com.example.scavenger;
+package com.example.scavenger.leaderboardfiles;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.scavenger.R;
 
 import java.util.ArrayList;
 import java.util.Comparator;
